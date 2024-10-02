d780683fa40cd3808e146e0cc110b28d4f8af093
Mario Zupan <mario@zupzup.org>
2024-10-02 12:28:23 +0200

    make generic over lanecount and see which one is fastest

--- benches/simd_bench.rs
+++ benches/simd_bench.rs
@@ -1,16 +1,16 @@
 #![feature(portable_simd)]
 use criterion::{criterion_group, criterion_main, Criterion};
 use num::SimdFloat;
-use std::simd::SupportedLaneCount;
 use std::simd::*;
-// TODO: make generic over SupportedLaneCount
-// => Benchmark which lane count makes the most sense
 
-pub fn simple_calc_simd(input: &[f32]) -> f32 {
-    let multiplicator = f32x4::splat(10.0);
+pub fn simple_calc_simd<const N: usize>(input: &[f32]) -> f32
+where
+    LaneCount<N>: SupportedLaneCount,
+{
+    let multiplicator: Simd<f32, N> = Simd::splat(10.0);
     let mut result: f32 = 0.0;
-    input.chunks(4).for_each(|chunk| {
-        let arr = f32x4::from_slice(chunk);
+    input.chunks(N).for_each(|chunk| {
+        let arr: Simd<f32, N> = Simd::from_slice(chunk);
         let mul = arr * multiplicator;
         let sum = mul.reduce_sum();
         result += sum;
@@ -38,14 +38,68 @@ fn crit_bench(c: &mut Criterion) {
     c.bench_function("normal", |b| b.iter(|| simple_calc(slice)));
 }
 
-fn crit_bench_simd(c: &mut Criterion) {
+fn crit_bench_simd_n_1(c: &mut Criterion) {
     let mut long_vec = vec![];
     for i in 0..(64 * 1000000) {
         long_vec.push(i as f32);
     }
     let slice = long_vec.as_slice();
-    c.bench_function("simd", |b| b.iter(|| simple_calc_simd(slice)));
+    c.bench_function("simd 1", |b| b.iter(|| simple_calc_simd::<1>(slice)));
 }
 
-criterion_group!(benches, crit_bench, crit_bench_simd);
+fn crit_bench_simd_n_2(c: &mut Criterion) {
+    let mut long_vec = vec![];
+    for i in 0..(64 * 1000000) {
+        long_vec.push(i as f32);
+    }
+    let slice = long_vec.as_slice();
+    c.bench_function("simd 2", |b| b.iter(|| simple_calc_simd::<2>(slice)));
+}
+
+fn crit_bench_simd_n_4(c: &mut Criterion) {
+    let mut long_vec = vec![];
+    for i in 0..(64 * 1000000) {
+        long_vec.push(i as f32);
+    }
+    let slice = long_vec.as_slice();
+    c.bench_function("simd 4", |b| b.iter(|| simple_calc_simd::<4>(slice)));
+}
+
+fn crit_bench_simd_n_8(c: &mut Criterion) {
+    let mut long_vec = vec![];
+    for i in 0..(64 * 1000000) {
+        long_vec.push(i as f32);
+    }
+    let slice = long_vec.as_slice();
+    c.bench_function("simd 8", |b| b.iter(|| simple_calc_simd::<8>(slice)));
+}
+
+fn crit_bench_simd_n_16(c: &mut Criterion) {
+    let mut long_vec = vec![];
+    for i in 0..(64 * 1000000) {
+        long_vec.push(i as f32);
+    }
+    let slice = long_vec.as_slice();
+    c.bench_function("simd 16", |b| b.iter(|| simple_calc_simd::<16>(slice)));
+}
+
+fn crit_bench_simd_n_32(c: &mut Criterion) {
+    let mut long_vec = vec![];
+    for i in 0..(64 * 1000000) {
+        long_vec.push(i as f32);
+    }
+    let slice = long_vec.as_slice();
+    c.bench_function("simd 32", |b| b.iter(|| simple_calc_simd::<32>(slice)));
+}
+
+criterion_group!(
+    benches,
+    crit_bench,
+    crit_bench_simd_n_1,
+    crit_bench_simd_n_2,
+    crit_bench_simd_n_4,
+    crit_bench_simd_n_8,
+    crit_bench_simd_n_16,
+    crit_bench_simd_n_32
+);
 criterion_main!(benches);
--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,26 @@ use std::simd::*;
 
 fn main() {
     let mut long_vec = vec![];
-    for i in 0..(4096) {
+    for i in 0..4096 {
         long_vec.push(i as f32);
     }
     // TODO: why do they have different results??
     // floating point arithmetic difference
     simple_calc(long_vec.as_slice());
-    simple_calc_simd(long_vec.as_slice());
+    simple_calc_simd::<8>(long_vec.as_slice());
 }
 
-pub fn simple_calc_simd(input: &[f32]) {
-    let multiplicator = f32x8::splat(1.3);
+pub fn simple_calc_simd<const N: usize>(input: &[f32])
+where
+    LaneCount<N>: SupportedLaneCount,
+{
+    let multiplicator: Simd<f32, N> = Simd::splat(1.3);
     let mut result: f32 = 0.0;
     let mut i = 0;
     let mut last = 0.0;
     input.chunks(8).for_each(|chunk| {
         i += 1;
-        let arr = f32x8::from_slice(chunk);
+        let arr: Simd<f32, N> = Simd::from_slice(chunk);
         let mul = arr * multiplicator;
         // println!("simd: {}", result);
         result += mul.reduce_sum();
